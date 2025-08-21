3908ae367e721a94748150433de5316774b69563
XomCreate <priscaojimba15@gmail.com>
2025-08-21 11:34:24 -0700

    update

--- src/MainComponets/Gallery.jsx
+++ src/MainComponets/Gallery.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GalleryA from '../GalleryComponets/GalleryA'
 
 function Gallery() {
   return (
     <>
     <div>
-      
+      <GalleryA/>
     </div>
     </>
   )
